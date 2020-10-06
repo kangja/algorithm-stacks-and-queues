e096b04facede5ecaece80d6bec11907f5091596
hi <kangja1996@gmail.com>
2020-10-05 21:42:27 -0400

    finished

--- stack-data-structure.js
+++ stack-data-structure.js
@@ -28,13 +28,44 @@ class Stack {
   peek() {
     console.log(`Top element is ${this.items[this.count - 1]}`)
     return this.items[this.count - 1] //this is top level item 
+  }
 
+  //check if stack is empty
+  isEmpty() { //check if the count equals to 0
+    console.log(this.count == 0 ? "Stack is empty" : "Stack is NOT empty");
+    return this.count == 0  // will give us true or false 
+  }
 
+  //check size of stack 
+  size() { //we just need to return the count 
+    console.log(`${this.count} element in stack`)
+    return this.count
+  }
+
+  //print elements in stack 
+  print() {
+    let str = "";
+    for (let i = 0; i < this.count; i++) {
+      str += this.items[i] + " " //we want to append to it 
+    }
+    return str
+  }
+
+  //clear stack 
+  clear() {
+    this.items = []
+    this.count = 0
+    console.log("Stack cleared")
+    return this.items
   }
 }
 
 //initialize variable to our class. Create stack object. 
 const stack = new Stack
+
+stack.isEmpty() //we are putting on top because we know that the list is empty as of now. 
+// Stack is empty
+
 
 //adding on to stacks using .push
 stack.push(100)
@@ -53,6 +84,10 @@ stack.push(300)
 // 200 added to 1       //200 is added to 1st position
 // 300 added to 2       //300 is added to 2nd position 
 
+console.log(stack.print())
+// 100 200 300 
+
+
 stack.pop()   //will remove the most top item, which will be the last one added. The last we added will be the top. 
 // 100 added to 0
 // 200 added to 1
@@ -66,7 +101,24 @@ stack.pop() //if we call pop again, it will remove 200.
 // 300 removed
 // 200 removed
 
-stack.pop()
+stack.clear()
+// Stack cleared
+
+console.log(stack.print())
+// 100
+
+
+stack.size()
+// Stack is empty
+// 100 added to 0
+// 200 added to 1
+// Top element is 200
+// 300 added to 2
+// 300 removed
+// 200 removed
+// 1 element in stack   //there is only 1 element because we got rid of 300 and 200. 
+
+// stack.pop()
 // 100 added to 0
 // 200 added to 1
 // 300 added to 2
@@ -74,7 +126,7 @@ stack.pop()
 // 200 removed
 // 100 removed 
 
-console.log(stack.pop()) //if we call it again, we get undefined because it's empty. 
+// console.log(stack.pop()) //if we call it again, we get undefined because it's empty. 
 // 100 added to 0
 // 200 added to 1
 // 300 added to 2
@@ -88,3 +140,14 @@ console.log(stack.pop()) //if we call it again, we get undefined because it's em
 // 0 because it gives me the position of 0. it's saying that number 0 is at 0th position. 
 
 
+stack.isEmpty()
+// Stack is empty
+// 100 added to 0
+// 200 added to 1
+// Top element is 200
+// 300 added to 2
+// 300 removed
+// 200 removed
+// 100 removed
+// undefined
+// Stack is empty
